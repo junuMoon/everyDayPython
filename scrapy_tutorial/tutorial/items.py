9d0a22ad64469925bbd8a7a97c23575c4537e49d
Junu Moon <francomoon7@gmail.com>
2021-03-23 17:14:14 +0900

    scrapy_tutorial: make item with loader

--- scrapy_tutorial/tutorial/items.py
+++ scrapy_tutorial/tutorial/items.py
@@ -1,12 +1,47 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+from scrapy.item import Item, Field
+from scrapy.loader.processors import MapCompose, TakeFirst
+from datetime import datetime
 
-import scrapy
+def remove_quotes(text):
+    """ strip the unicode quotes """
+    text = text.strip(u'\u201c')
+    text = text.strip(u'\u201d')
+    return text
 
 
-class TutorialItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+def convert_date(date_text):
+    """ convert string March 14 to datetime object """
+    return datetime.strptime(date_text, "%B %d, %Y" )
+
+
+def parse_location(location_text):
+    """ 
+    parse location in Born information
+    This simply remove "in" in string.
+    You can further parse city, state, country, etc.
+    """
+    return location_text[3:]
+    
+
+class QuoteItem(Item):
+    quote_content = Field(
+        input_processor=MapCompose(remove_quotes),
+        output_processor=TakeFirst()
+    )
+    author_name = Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    author_birthdate = Field(
+        input_processor=MapCompose(convert_date),
+        output_processor=TakeFirst()
+    )
+    author_bornlocation = Field(
+        input_processor=MapCompose(parse_location),
+        output_processor=TakeFirst()
+    )
+    author_bio = Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    tags = Field()
--- scrapy_tutorial/tutorial/spiders/quotes_spider.py
+++ scrapy_tutorial/tutorial/spiders/quotes_spider.py
@@ -1,31 +1,37 @@
 import scrapy
+from tutorial.items import QuoteItem
+from scrapy.loader import ItemLoader
+
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
+    allowed_domains = ["toscrape.com"]
     start_urls = ['http://quotes.toscrape.com']
 
     def parse(self, response):
-        self.logger.info('Hello, this is my first spider')
+        self.logger.info(f'Parse function called on {response.url}')
         quotes = response.css('div.quote')
-        for quote in quotes:
-            yield {
-                'text': quote.css('.text::text').get(),
-                'author': quote.css('.author::text').get(),
-                'tags': quote.css('.tag::text').getall()
-            }
 
-        author_url = response.css('.author + a::attr(href)').get()
-        self.logger.info('get author page')
-        yield response.follow(author_url, callback=self.parse_author)
+        for quote in quotes:
+            loader = ItemLoader(item=QuoteItem(), selector=quote)
+            loader.add_css('quote_content', '.text::text')
+            loader.add_css('tags', '.tag::text')
+            
+            quote_item = loader.load_item()
+            author_url = quote.css('.author + a::attr(href)').get()
+
+            yield response.follow(author_url,
+                                 callback=self.parse_author,
+                                 meta={'quote_item': quote_item})
 
         for a in response.css('li.next a'):  # <class 'scrapy.selector.unified.SelectorList'>
             yield response.follow(a, callback=self.parse)  # <class 'scrapy.selector.unified.Selector'>
 
     def parse_author(self, response):
-        yield {
-            'author_name': response.css('.author-title::text').get(),
-            'author_birthday': response.css('.author-born-date::text').get(),
-            'author_bornlocation': response.css('.author-born-location::text').get(),
-            'author_bio': response.css('.author-description::text').get()
-        }
+        quote_item = response.meta['quote_item']
+        loader = ItemLoader(item=quote_item, response=response)
+        loader.add_css('author_name', '.author-title::text')
+        loader.add_css('author_birthdate', '.author-born-date::text')
+        loader.add_css('author_bio', '.author-description::text')
+        loader.add_css('author_bornlocation', '.author-born-location::text')
+        yield loader.load_item()
