9ea3c40c3eef3b9847cf06d2796a865bfca266f3
Junu Moon <francomoon7@gmail.com>
2021-03-26 16:13:53 +0900

    Dynamic scrape: get proxy list with cdn notes.md: what is cdn

--- dynamic_scrape/proxy.py
+++ dynamic_scrape/proxy.py
@@ -1,38 +1,24 @@
 import requests
-from bs4 import BeautifulSoup
-from random import choice
 
 
-def get_proxies():
-    proxy_url = "https://github.com/clarketm/proxy-list/blob/master/proxy-list-raw.txt"
-    r = requests.get(proxy_url)
-    soup = BeautifulSoup(r.content, "html.parser").select('td.blob-code.blob-code-inner')
-    proxies = [proxy.text for proxy in soup]
-
-    return proxies
+def get_proxy_list():
+    proxy_list = requests.get('https://cdn.jsdelivr.net/gh/clarketm/proxy-list@master/proxy-list-raw.txt').text.split('\n')
+    return proxy_list
 
 
-def get_random_proxy(proxies):
-    return {"https": choice(proxies)}
-
-
-proxies = get_proxies()
-
-
-def get_working_proxies():
+def get_working_proxies(proxy_list):
     working_proxies = list()
-    for i in range(30):
-        proxy = get_random_proxy(proxies)
-        print(f"using {proxy}...")
-        try:
-            r = requests.get("https://www.google.com", proxies=proxy, timeout=2)
-            if r.status_code == 200:
-                working_proxies.append(proxy)
-        except:
-            pass
+    for proxy in proxy_list:
+        if proxy != '':
+            try:
+                r = requests.get("https://google.com", proxies={'https': proxy}, timeout=2)
+                if r.status_code == 200:
+                    print(f"{proxy} is working!")
+                    working_proxies.append(proxy)
+            except:
+                print(f"{proxy} fails.")
+                pass
     return working_proxies
 
-url = "https://pexels.com"
-proxy = choice(get_working_proxies())
 
-requests.get(url, proxies=proxy)
+print(get_working_proxies(get_proxy_list()))
