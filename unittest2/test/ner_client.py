8860346e7acbc642fa8fe96add7451be05da22c4
Junu Moon <francomoon7@gmail.com>
2021-03-30 11:04:36 +0900

    model mocked. It works well. Now need model to import on NerClient

--- unittest2/test/ner_client.py
+++ unittest2/test/ner_client.py
@@ -1,8 +1,24 @@
-# TODO: ner model import
+import re
+
 
 class NerClient:
+
     def __init__(self):
-        pass
+        self.model = {
+            'Franco': 'Person',
+            'Madrid': 'Location'
+        }
 
     def get_ents(self, sent):
-        return {}
+        sent = self.sent_cleaning(sent)
+        words = sent.split(' ')
+        ents = []
+        for word in words:
+            if word in self.model.keys():
+                ents.append({word: self.model[word]})
+        return ents
+
+    @staticmethod
+    def sent_cleaning(sent):
+        sent = re.sub(r"[^a-zA-Z\s]", "", sent)
+        return sent
--- unittest2/test/test_ner_client.py
+++ unittest2/test/test_ner_client.py
@@ -4,20 +4,38 @@ from ner_client import NerClient
 
 class TestNerClient(unittest.TestCase):
 
-    def test_ner_client_returns_dictionary_given_empty_sent(self):
+    def test_ner_client_returns_empty_list_given_empty_sent(self):
         model = NerClient()
         sent = ''
         ents = model.get_ents(sent)
-        self.assertIsNotNone(ents)
+        self.assertIsInstance(ents, list)
 
-    def test_ner_clinet_returns_entities_dictionary_given_nonempty_sent(self):
+    def test_ner_client_returns_entity_dictionary_list_given_multiple_known_entity_sent(self):
         model = NerClient()
         sent = 'Franco lives in Madrid.'
         ents = model.get_ents(sent)
-        expected_result = {
-            'ents': [
-                {'Franco': 'Person'},
-                {'Madrid': 'Location'}
-            ]
-        }
+        expected_result = [
+            {'Franco': 'Person'},
+            {'Madrid': 'Location'}
+        ]
         self.assertEqual(ents, expected_result)
+
+    def test_ner_client_replace_all_symbols(self):
+        model = NerClient()
+        sent = 'Franco lives% in Madrid.'
+        sent_cleansed = model.sent_cleaning(sent)
+        expected_result = "Franco lives in Madrid"
+        self.assertEqual(sent_cleansed, expected_result)
+
+    def test_ner_client_fetch_entity_dict_list_from_model_given_known_multiple_entity_sent(self):
+        model = NerClient()
+        sent = 'Franco lives in Madrid.'
+        ents = model.get_ents(sent)
+        expected_result = [
+            {'Franco': 'Person'},
+            {'Madrid': 'Location'}
+        ]
+        self.assertEqual(ents, expected_result)
+
+
+
