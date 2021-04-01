926c6c30ced5cf2eb9f082702f65d0b42dd99bab
Junu Moon <francomoon7@gmail.com>
2021-04-01 10:34:42 +0900

    tested: one route 통합 need to: route revise     2. jsonrify?     3. error hadnling

--- unittest2/app.py
+++ unittest2/app.py
@@ -4,24 +4,19 @@ from ner_client import NerClient
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/ner', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def ner():
-    try:
-        sent = request.form['sentence']
-        if len(sent) == 0:
-            return render_template('home.html')
-        else:
+    if request.method == 'GET':
+        return render_template('home.html')
+    if request.method == 'POST':
+        try:
+            sent = request.form['sentence']
             model = NerClient()
             ents = model.get_ents(sent)['ents']
             return render_template('home.html', ents=ents)
-    except Exception:
-        abort(404)
-        return ""
+        except Exception:
+            abort(404)
+            return ""
 
 
 if __name__ == "__main__":
