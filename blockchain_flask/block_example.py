e6f755ed70b86059c60334f330c545c26b70618d
Junu Moon <francomoon7@gmail.com>
2021-04-16 16:56:33 +0900

    block structure

---
+++ blockchain_flask/block_example.py
@@ -0,0 +1,13 @@
+block = {
+    'index': 1,
+    'timestamp': 'timestamp',
+    'transactions': [
+        {
+            'sender': 'hash',
+            "recipient": 'hash',
+            'amount': 5,
+        },
+    ],
+    'proof': 'proof_id',
+    'previous_hash': 'hash'
+}
