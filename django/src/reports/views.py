4a39ee0217573b80c720fe5129da524a81b7ee49
Junu Moon <francomoon7@gmail.com>
2021-04-20 13:40:52 +0900

    create report object with chart image

--- django/src/reports/views.py
+++ django/src/reports/views.py
@@ -3,6 +3,7 @@ from profiles.models import Profile
 from reports.models import Report
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from reports.utils import get_report_image
 
 
 # Create your views here.
@@ -10,18 +11,20 @@ def create_report_view(request):
     if request.is_ajax():
         name = request.POST.get('name')
         remarks = request.POST.get('remarks')
-        img = request.POST.get('image') # TODO: from .utils import get_report_image
+        image = request.POST.get('image')
+        
+        img = get_report_image(image)
         
         author_user = User.objects.get(username='test_user')
         author_profile = Profile.objects.get(user=author_user) #TODO: need proper user input
         
-        report = Report(
+        Report.objects.create(
             name = name,
             image = img,
             remarks = remarks,
             author = author_profile
         )
-        report.save()
+
         return JsonResponse({'msg': 'success'})
     return JsonResponse({})
         
