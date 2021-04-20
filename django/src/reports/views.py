162c32e178a94d7d6edf4e2eab30228d1daec426
Junu Moon <francomoon7@gmail.com>
2021-04-20 13:46:44 +0900

    another way to create model instance with instance save function

--- django/src/reports/views.py
+++ django/src/reports/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from profiles.models import Profile
 from reports.models import Report
+from reports.forms import ReportForm
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from reports.utils import get_report_image
@@ -8,22 +9,28 @@ from reports.utils import get_report_image
 
 # Create your views here.
 def create_report_view(request):
+    form = ReportForm(request.POST or None)
     if request.is_ajax():
-        name = request.POST.get('name')
-        remarks = request.POST.get('remarks')
+        # name = request.POST.get('name')
+        # remarks = request.POST.get('remarks')
         image = request.POST.get('image')
-        
         img = get_report_image(image)
         
         author_user = User.objects.get(username='test_user')
         author_profile = Profile.objects.get(user=author_user) #TODO: need proper user input
         
-        Report.objects.create(
-            name = name,
-            image = img,
-            remarks = remarks,
-            author = author_profile
-        )
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.image = img
+            instance.author = author_profile
+        instance.save()
+        
+        # Report.objects.create(
+        #     name = name,
+        #     image = img,
+        #     remarks = remarks,
+        #     author = author_profile
+        # )
 
         return JsonResponse({'msg': 'success'})
     return JsonResponse({})
