9e5367dd8cc32da44d85eb6646d152c606ef8cf0
Junu Moon <francomoon7@gmail.com>
2021-04-22 10:41:04 +0900

    fixed: creating duplicated sales object - get_or_create(default={})

--- django/src/reports/views.py
+++ django/src/reports/views.py
@@ -1,21 +1,27 @@
+import csv
+from datetime import datetime
 from os import read
+
+from customers.models import Customer
 from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.template.loader import get_template
-from django.views.generic import DetailView, ListView, TemplateView
-from profiles.models import Profile
+from django.utils import timezone
 from django.utils.dateparse import parse_date
-
+from django.views.generic import DetailView, ListView, TemplateView
+from products.models import Product
+from profiles.models import Profile
+from sales.models import CSV, Position, Sale
 from xhtml2pdf import pisa
-import csv
 
 from reports.forms import ReportForm
 from reports.models import Report
 from reports.utils import get_report_image
 
-from sales.models import Sale, Position, CSV
-from products.models import Product
+# from datetime import datetime
+
+
 
 
 class ReportListView(ListView):
@@ -38,17 +44,35 @@ def csv_upload_view(request):
             reader = csv.reader(f)
             reader.__next__() # skip the first row
             for row in reader:
-                transaction_id = row[1]
-                product = row[2]
+                transaction_id = row[1].strip()
+                product = row[2].strip()
                 quantity = int(row[3])
-                customer = row[4]
-                date = parse_date(row[6])
+                customer = row[4].strip()
+                date = parse_date(row[5].strip())
+                date = datetime.combine(date, datetime.min.time())   
+                # date = timezone.make_aware(date)
+                # date = datetime.strptime(row[5].strip())
                 
                 try:
-                    product_obj = Product.objects.get(name=product)
+                    product_obj = Product.objects.get(name__iexact=product)
                 except Product.DoesNotExist:
                     product_obj = None
                 
+                if product_obj is not None:
+                    customer_obj, _ = Customer.objects.get_or_create(name=customer)  # return object, bool
+                    salesman = User.objects.get(username='test_user')
+                    salesman_obj = Profile.objects.get(user=salesman) #TODO: need proper user input
+                    position_obj, _ = Position.objects.get_or_create(product=product_obj,
+                                                                    quantity=quantity,
+                                                                    created=date)
+                    sale_obj, _ = Sale.objects.get_or_create(transaction_id=transaction_id,
+                                                             defaults={
+                                                                 'customer': customer_obj,
+                                                                 'salesman': salesman_obj,
+                                                                 'created': date})
+                    sale_obj.positions.add(position_obj)
+                    sale_obj.save()
+                    
     return HttpResponse()
 
 # Create your views here.
