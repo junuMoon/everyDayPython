df9dc356d9be62b777077ff0be9a7669e8aad6e0
Junu Moon <francomoon7@gmail.com>
2021-04-24 13:53:05 +0900

    ignore: db build: skeleton api

--- data_structure/notes.md
+++ data_structure/notes.md
@@ -1,4 +1,5 @@
 # Data Structure w/ Flask
 
 - ORM: Object Relational Mapper
-
+- route: A decorator that is used to register a *view function* for a given URL rule.
+- payload: refers to an integral part of each unit of data being transmitted. It is part of the unit data that carries the real message that an app or system needs for it to act.
--- data_structure/server.py
+++ data_structure/server.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 # app
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://sqlitedb.file"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data_structure.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # configure sqlite3 to enforce foreign key contraints
@@ -32,3 +32,52 @@ class User(db.Model):
     phone = db.Column(db.String(50))
     posts = db.relationship("BlogPost")
 
+
+class BlogPost(db.Model):
+    __tablename__ = 'blog_post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    
+    
+# routes
+@app.route("/user", methods=["POST"])
+def create_user():
+    pass
+
+@app.route("/user/descending_id", methods=["GET"])
+def get_all_user_descending():
+    pass
+
+@app.route("/user/ascending_id", methods=["GET"])
+def get_all_user_ascending():
+    pass
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    pass
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    pass
+
+@app.route("/blog_post/<user_id>", methods=["POST"])
+def create_post(user_id):
+    pass
+
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_one_posts(blog_post_id):
+    pass
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_all_posts(user_id):
+    pass
+
+@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
+def delete_posts(blog_post_id):
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True) 
