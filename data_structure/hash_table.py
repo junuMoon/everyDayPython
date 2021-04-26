fdcfe127a659d3cfbff50117fd91cefacfbbf863
Junu Moon <francomoon7@gmail.com>
2021-04-26 14:46:11 +0900

    code cleansed

--- data_structure/hash_table.py
+++ data_structure/hash_table.py
@@ -17,9 +17,9 @@ class HashTable:
         hash_value = 0
         for i in key:
             hash_value += ord(i)
-            print(f'1: {hash_value}')
+            # print(f'1: {hash_value}')
             hash_value = (hash_value * ord(i)) % self.table_size
-            print(f'2: {hash_value}')
+            # print(f'2: {hash_value}')
         return hash_value
     
     def add_key_value(self, key, value):
@@ -34,42 +34,41 @@ class HashTable:
             
     def get_value(self, key):
         hashed_key = self.custom_hash(key)
-        if self.hash_table[hashed_key] is not None:
-            node = self.hash_table[hashed_key]
-            if node.next_node is None:
-                return node.data.value
-            while node.next_node:
-                if key == node.data.key:
-                    return node.data.value
-                node = node.next_node
-                
+        if self.hash_table[hashed_key] is None:
+            raise AttributeError('no node mapped')
+        
+        node = self.hash_table[hashed_key]
+        while node.next_node:
             if key == node.data.key:
-                return node.data.key
-        return None
+                return node.data.value
+            node = node.next_node
+        if key == node.data.key:
+            return node.data.key
+        raise AttributeError('No node with the key')
     
     def print_table(self):
         print("{")
-        for i, val in enumerate(self.hash_table):
-            if val is not None:
-                l_list_string = ""
-                node = val
-                if node.next_node:
-                    while node.next_node:
-                        l_list_string += (
-                            str(node.data.key) + " : " + str(node.data.value) + " --> "
-                        )
-                        node = node.next_node
-                    l_list_string += (
-                        str(node.data.key) + " : " + str(node.data.value) + " -->None"
-                    )
-                    print(f"\t[{i}] {l_list_string}")
-                else:
-                    print(f"\t[{i}] {val.data.key} : {val.data.value}")
+        for i, node in enumerate(self.hash_table):
+            if node is not None:
+                ll_string = ""
+                while node.next_node:
+                    node_string = f"{node.data.key} : {node.data.value}"
+                    ll_string += (node_string + " --> ")
+                    node = node.next_node
+                node_string = f"{node.data.key} : {node.data.value}"
+                ll_string += (node_string + " --> None")
+                print(f"\t[{i}]" + ll_string)
             else:
-                print(f"\t[{i}] {val}")
+                print(f"\t[{i}] {node}")
         print("}")
                     
-                        
+ht = HashTable(3)                       
+ht.add_key_value('apple', 'iphone')
+ht.add_key_value('samsung', 's21')
+ht.add_key_value('one', 'two')
+ht.add_key_value('hi', 'there')
+ht.add_key_value('hello', 'world')
+ht.add_key_value('python', 'pythonista')
                 
                     
         
