cf571bfc197afa20a8c0063f5ae0787274db3b1f
Junu Moon <francomoon7@gmail.com>
2021-04-28 18:48:34 +0900

    build model in products

--- microservice/admin/products/models.py
+++ microservice/admin/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Prodcut(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200, default='default.jpg')
+    likes = models.PositiveIntegerField(default=0)
+    
+
+class User(models.Model):
+    pass
